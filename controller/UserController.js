8910311b04abb468856fa5e4d26a6b69e568567d
Robson47 <clonederobson@gmail.com>
2024-02-20 22:22:08 -0300

    alterações e novas rotas

--- controller/UserController.js
+++ controller/UserController.js
@@ -13,14 +13,33 @@ app.get('/', (req, res) => {
 });
 
 /* Rota Privada */
-app.get('/user/:id', async (req, res) => {
+app.get('/user/:id', checkToken, async (req, res) => {
     const id = req.params.id;
     const user = await User.findById(id, '-password');
 
     if (!user) {
         return res.status(404).json({ msg: 'Usuário não encontrado!' });
     };
+
+    res.status(200).json({ user });
 });
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ msg: 'Acesso negado!' })
+    };
+
+    try {
+        const secret = process.env.SECRET;
+        jwt.verify(token, secret);
+        next();
+    } catch (error) {
+        res.status(400).json({ msg: 'Token inválido!' });
+    };
+};
 
 /* Rota de Registro */
 app.post('/auth/register', async (req, res) => {
